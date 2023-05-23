add331533be28ff0d666f8e688cd12232ef5830a
Marco <tignanellimarco@gmail.com>
2023-05-23 19:53:58 -0300

    Basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import github from '../assets/contact/github.svg';
+import linkedin from '../assets/contact/linkedin.svg';
+import gmail from '../assets/contact/gmail.svg';
+
+export default function Footer() {
+    return (
+        <footer className='flex justify-between'>
+            <a href="/gmail" target='_blank' className='flex'>
+                <img src={gmail} alt="gmail profile link" />
+                tignanellimarco@gmail.com
+            </a>
+
+            <div className='flex'>
+                <a href="https://www.linkedin.com/in/marco-tignanelli/" target='_blank'>
+                    <img src={linkedin} className='w-12' alt="linkedin profile link" />
+                </a>
+                <a href="https://github.com/Mark-kus" target='_blank'>
+                    <img src={github} className='w-12' alt="github profile link" />
+                </a>
+            </div>
+
+        </footer>
+    )
+}
