47f4c558564345ef15399f5434c3668b90cfac62
Marco <tignanellimarco@gmail.com>
2023-05-23 19:54:06 -0300

    Basic Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <nav>
+            <a href="">About</a>
+            <a href="">Skills</a>
+            <a href="">Projects</a>
+            <a href="">Contact</a>
+        </nav>
+    )
+}
