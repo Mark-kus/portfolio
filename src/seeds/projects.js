574ee1f0dfb36993059b70b803f106e3537512ac
Marco <tignanellimarco@gmail.com>
2023-05-24 22:01:09 -0300

    project seeds

---
+++ src/seeds/projects.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: "Hueney Ruca",
+        image: "https://placehold.co/600x400",
+    },
+    {
+        name: "Dogs API",
+        image: "https://placehold.co/600x400",
+    }
+]
