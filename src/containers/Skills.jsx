f399d7c5b0099b63a4cf6501c0677c5497e57394
Marco <tignanellimarco@gmail.com>
2023-05-26 12:29:21 -0300

    restructure

--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -1,7 +1,7 @@
 import Skill from "../components/Skill"
 import html from '../assets/tech/html.svg'
 import css from '../assets/tech/css.svg'
-import javscript from '../assets/tech/javascript.svg'
+import javascript from '../assets/tech/javascript.svg'
 import nextjs from '../assets/tech/nextjs.svg'
 import nodejs from '../assets/tech/nodejs.svg'
 import postgres from '../assets/tech/postgres.svg'
@@ -11,7 +11,37 @@ import sequelize from '../assets/tech/sequelize.svg'
 import mongodb from '../assets/tech/mongodb.svg'
 
 export default function Skills() {
-    const techSkills = [html, css, javscript, nodejs, react, nextjs, redux, sequelize, postgres, mongodb]
+    const techSkills = [{
+        tech: html,
+        name: 'HTML'
+    }, {
+        tech: css,
+        name: 'CSS'
+    }, {
+        tech: javascript,
+        name: 'Javascript'
+    }, {
+        tech: nodejs,
+        name: 'Node.JS'
+    }, {
+        tech: react,
+        name: 'React.JS'
+    }, {
+        tech: nextjs,
+        name: 'Next.JS'
+    }, {
+        tech: redux,
+        name: 'Redux.JS'
+    }, {
+        tech: sequelize,
+        name: 'Sequelize'
+    }, {
+        tech: postgres,
+        name: 'PostgreSQL'
+    }, {
+        tech: mongodb,
+        name: 'MongoDB'
+    }]
     return (
         <section className="flex flex-col md:m-10">
             <header className="text-lg">Tech Skills</header>
