57115f56cda46b09c764fbdfc2c8ac8363a9b336
Marco <tignanellimarco@gmail.com>
2023-05-26 12:42:15 -0300

    hover shows skillName

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
+
 export default function Skill({ skill }) {
+    const [name, setName] = useState('');
+    const showName = () => setName(skill.name);
+    const hideName = () => setName('');
     return (
-        <figure>
-            <img src={skill} alt={skill.slice(-4)} 
-            className="w-12 md:w-24"/>
+        <figure className="flex justify-center items-center" >
+            <img src={skill.tech} alt={skill.name}
+                className="w-12 md:w-24 hover:blur-xl transition-all"
+                onMouseEnter={showName}
+                onMouseLeave={hideName} />
+            <h1 className="absolute pointer-events-none">{name}</h1>
         </figure>
     )
 }
