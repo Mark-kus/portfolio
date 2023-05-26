5c3f2f2a447635481e639859ab18ac56c6e3c0b3
Marco <tignanellimarco@gmail.com>
2023-05-26 12:44:43 -0300

    spacing

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -5,12 +5,12 @@ export default function Skill({ skill }) {
     const showName = () => setName(skill.name);
     const hideName = () => setName('');
     return (
-        <figure className="flex justify-center items-center" >
+        <figure className="flex justify-center items-center m-2" >
             <img src={skill.tech} alt={skill.name}
                 className="w-12 md:w-24 hover:blur-xl transition-all"
                 onMouseEnter={showName}
                 onMouseLeave={hideName} />
-            <h1 className="absolute pointer-events-none">{name}</h1>
+            <h1 className="absolute pointer-events-none text-xs md:text-base">{name}</h1>
         </figure>
     )
 }
