d730dc8dbb9e8846d0d2eb89f62d5ceff7dc4e94
Marco <tignanellimarco@gmail.com>
2023-05-26 12:32:46 -0300

    misc

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,10 @@ export default function About() {
                 When I become an adult, I would like to be a genius, millionaire, playboy, philanthropist. Yes, like him.
                 But just in case it doesn't happen, I armed myself with knowledge on front and back end, to develop amazing web
                 applications. <br />
-                I became a full stack web developer, graduated from <a href="https://www.soyhenry.com/" target='_blank' className='text-yellow-500'>SoyHenry</a>,
-                and studied at <a href="https://www.udemy.com/" target='_blank' className='text-violet-500'>Udemy</a>, to later become self-taught. <br />
+                I became a full stack web developer, graduated 
+                from <a href="https://www.soyhenry.com/" target='_blank' className='text-yellow-500'>SoyHenry</a>,
+                and studied at <a href="https://www.udemy.com/" target='_blank' className='text-violet-500'>Udemy</a>, 
+                to later become self-taught. <br />
             </article>
         </article>
     )
