26e0ac998ff81d38c62811a3e6c89ef091aeb744
Marco <tignanellimarco@gmail.com>
2023-05-26 11:45:35 -0300

    self description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,16 @@ import profile from '../assets/profile.png'
 
 export default function About() {
     return (
-        <article className='flex flex-col items-center m-4 min-h-screen
+        <article className='flex flex-col items-center m-10 min-h-screen
         md:min-h-0 md:flex-row'>
-            <img src={profile} alt="Marco image" className='w-5/6 md:w-80' />
+            <img src={profile} alt="Marco image" className='w-5/6 sm:w-80' />
             <article className='text-center md: m-6'>
                 <header className='text-3xl leading-relaxed'>Marco Tignanelli</header>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni officia fuga porro. Cum quisquam expedita laborum accusamus labore ipsa doloremque officiis, excepturi reprehenderit laudantium consequuntur voluptas. Repellat qui ratione quos!
-                Dolores quos animi ratione est maiores accusamus? Atque rem porro ducimus, beatae ut iste optio soluta dolorum fuga architecto mollitia sequi molestiae quibusdam? Labore consequuntur, fugit similique quasi soluta accusamus!
+                When I become an adult, I would like to be a genius, millionaire, playboy, philanthropist. Yes, like him.
+                But just in case it doesn't happen, I armed myself with knowledge on front and back end, to develop amazing web
+                applications. <br />
+                I became a full stack web developer, graduated from <a href="https://www.soyhenry.com/" target='_blank' className='text-yellow-500'>SoyHenry</a>,
+                and studied at <a href="https://www.udemy.com/" target='_blank' className='text-violet-500'>Udemy</a>, to later become self-taught. <br />
             </article>
         </article>
     )
--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -8,9 +8,10 @@ import postgres from '../assets/tech/postgres.svg'
 import react from '../assets/tech/react.svg'
 import redux from '../assets/tech/redux.svg'
 import sequelize from '../assets/tech/sequelize.svg'
+import mongodb from '../assets/tech/mongodb.svg'
 
 export default function Skills() {
-    const techSkills = [html, css, javscript, nodejs, react, nextjs, redux, postgres, sequelize]
+    const techSkills = [html, css, javscript, nodejs, react, nextjs, redux, sequelize, postgres, mongodb]
     return (
         <section className="flex flex-col md:m-10">
             <header className="text-lg">Tech Skills</header>
