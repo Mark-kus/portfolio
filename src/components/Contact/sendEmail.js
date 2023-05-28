109731c4fbc223a62e0388fa93daeb4414841c51
Marco <tignanellimarco@gmail.com>
2023-05-28 16:15:57 -0300

    email send function

--- src/components/Contact/sendEmail.js
+++ src/components/Contact/sendEmail.js
@@ -1,4 +1,16 @@
+import emailjs from "@emailjs/browser";
+const {
+    EMAIL_TEMPLATE,
+    EMAIL_SERVICE,
+    EMAIL_KEY
+} = import.meta.env
+
 export default (inputs, setInputs) => {
     console.log(inputs);
+    // emailjs.send(EMAIL_SERVICE, EMAIL_TEMPLATE, {
+    //     from_name: inputs.name,
+    //     message: inputs.message,
+    //     from_email: inputs.email,
+    // });
     setInputs({})
 }
