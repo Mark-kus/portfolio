e621fd81259b3abbe0f6414fa3da8795cbf0d5a6
Marco <tignanellimarco@gmail.com>
2023-05-28 16:43:19 -0300

    deploy and repo URLs seed

--- src/seeds/projects.js
+++ src/seeds/projects.js
@@ -4,13 +4,19 @@ export default [
     {
         name: "Hueney Ruca",
         image: hueneyRuca,
+        deploy: "https://hueney-ruca-henry.vercel.app/",
+        repo: "https://github.com/Mark-kus/hueney-ruca",
     },
     {
         name: "Dogs",
         image: "https://placehold.co/600x400",
+        deploy: "",
+        repo: "https://github.com/Mark-kus/dogs-api",
     },
     {
         name: "Rick and Morty",
         image: "https://placehold.co/600x400",
+        deploy: "",
+        repo: "https://github.com/Mark-kus/rick-and-morty-api",
     },
 ]
