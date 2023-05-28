fe973ab935c3c9c35fc83851f0d7e7f8684f5a51
Marco <tignanellimarco@gmail.com>
2023-05-28 16:27:21 -0300

    controlled form and email sending

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,9 +1,8 @@
-import emailjs from "@emailjs/browser";
 import sendEmail from "./sendEmail";
 import { useState } from "react";
 
 export default function ContactForm() {
-    const [inputs, setInputs] = useState({});
+    const [inputs, setInputs] = useState({ name: '', email: '', message: '' });
 
     const inputCls = "bg-slate-600 p-2 rounded"
 
@@ -15,25 +14,24 @@ export default function ContactForm() {
     const handleSubmit = (e) => {
         e.preventDefault()
         sendEmail(inputs, setInputs)
-        e.target.reset()
     };
 
     return (
-        <form onSubmit={handleSubmit} className="text-black flex flex-col w-2/3 mx-auto">
+        <form onSubmit={handleSubmit} className="flex flex-col w-2/3 mx-auto">
 
-            <label htmlFor="name">Nombre/Empresa</label>
+            <label htmlFor="name">Name</label>
             <input id="name" name="name" type="text" onChange={handleChange} value={inputs.name}
-            placeholder="Peter Pan / GarfioTech"
-            className={`${inputCls}`} />
+                placeholder="Peter Pan / GarfioTech"
+                className={`mb-2 ${inputCls}`} />
 
-            <label htmlFor="email">Email al que responder</label>
+            <label htmlFor="email">Email to respond</label>
             <input id="email" name="email" type="email" onChange={handleChange} value={inputs.email}
-            placeholder="wendyrecruiter@gmail.com"
-            className={`${inputCls}`} />
+                placeholder="wendyrecruiter@gmail.com"
+                className={`mb-2 ${inputCls}`} />
 
-            <label htmlFor="message">Mensaje</label>
+            <label htmlFor="message">Message</label>
             <textarea name="message" id="message" cols="30" rows="10" onChange={handleChange} value={inputs.message}
-            className={`resize-none rounded-b-none ${inputCls}`} placeholder="Hola! Me intereso uno de tus proyectos y..." />
+                className={`resize-none rounded-b-none ${inputCls}`} placeholder="Hi! I like your projects..." />
 
             <button className="p-2 bg-blue-400">Enviar</button>
 
