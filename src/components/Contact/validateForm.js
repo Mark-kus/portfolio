0ceafaa86f14ce390e042a78a7eed4f87aaa968a
Marco <tignanellimarco@gmail.com>
2023-05-30 20:56:31 -0300

    shorted error

--- src/components/Contact/validateForm.js
+++ src/components/Contact/validateForm.js
@@ -7,17 +7,17 @@ export default function validateForm(inputs) {
 
     if (inputs.name) {
         if (!regexName.test(inputs.name)) errors.name = "It shouldn't contain special characters"
-        else if (inputs.name.length > 50) errors.name = "The text is too long, summarize it"
+        else if (inputs.name.length > 50) errors.name = "Too long, summarize it"
     }
 
     if (inputs.email) {
         if (!regexEmail.test(inputs.email)) errors.email = 'Must be a valid email'
-        else if (inputs.name.length > 50) errors.name = "The text is too long, summarize it"
+        else if (inputs.name.length > 50) errors.name = "Too long, summarize it"
     }
 
     if (inputs.message) {
         if (!regexMessage.test(inputs.message)) errors.message = "It shouldn't contain special characters"
-        else if (inputs.name.length > 500) errors.name = "The text is too long, summarize it"
+        else if (inputs.name.length > 500) errors.name = "Too long, summarize it"
     }
 
     return errors
