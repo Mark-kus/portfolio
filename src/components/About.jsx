0d39f85e29e6d697ffa6a4888c65884e92f91db4
Marco <tignanellimarco@gmail.com>
2023-05-30 15:51:11 -0300

    misc changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,11 @@ import profile from '../assets/profile.png'
 
 export default function About() {
     return (
-        <article className='flex flex-col items-center m-10 min-h-screen
-        md:min-h-0 md:flex-row'>
+        <article className='flex flex-col items-center m-10 md:flex-row'>
             <img src={profile} alt="Marco image" className='w-5/6 sm:w-80 rounded-full' />
             <article className='text-center md: m-6'>
                 <header className='text-3xl leading-relaxed'>Marco Tignanelli</header>
-                When I become an adult, I would like to be a genius, millionaire, playboy, philanthropist. Yes, like him.
+                When I become an adult, I would like to be a genius, millionaire, playboy, philanthropist.
                 But just in case it doesn't happen, I armed myself with knowledge on front and back end, to develop amazing web
                 applications. <br />
                 I became a full stack web developer, graduated 
