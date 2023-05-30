b7f276fb6b843b593a59b4c1798b156659604863
Marco <tignanellimarco@gmail.com>
2023-05-30 14:44:40 -0300

    seeds re-added

---
+++ src/seeds/projects.js
@@ -0,0 +1,24 @@
+import hueneyRuca from '../assets/projects/hueneyRuca.jpeg'
+import rym from '../assets/projects/rym.jpeg'
+import dogApi from '../assets/projects/dogApi.jpeg'
+
+export default [
+    {
+        name: "Hueney Ruca",
+        image: hueneyRuca,
+        deploy: "https://hueney-ruca-henry.vercel.app/",
+        repo: "https://github.com/Mark-kus/hueney-ruca",
+    },
+    {
+        name: "Dogs",
+        image: dogApi,
+        deploy: "",
+        repo: "https://github.com/Mark-kus/dogs-api",
+    },
+    {
+        name: "Rick and Morty",
+        image: rym,
+        deploy: "",
+        repo: "https://github.com/Mark-kus/rick-and-morty-api",
+    },
+]
