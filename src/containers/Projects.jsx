1b6c290773df9ec60adf534ea29de361796b5139
Marco <tignanellimarco@gmail.com>
2023-05-30 15:09:40 -0300

    responsive slider

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,6 +1,7 @@
 import Project from "../components/Project";
 import myProjects from "../seeds/projects.js"
 import Slider from "react-slick";
+import swipe from '../assets/swipe.svg'
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -14,9 +15,16 @@ export default function Projects() {
         centerPadding: "200px",
         responsive: [
             {
-                breakpoint: 600,
+                breakpoint: 639,
                 settings: {
                     slidesToShow: 1,
+                    centerMode: false,
+                }
+            },
+            {
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 2,
                     centerMode: false,
                 }
             }
@@ -25,11 +33,14 @@ export default function Projects() {
 
     return (
         <section className="flex flex-col">
-            <header className="md:mx-10">Projects</header>
-            <article className="">
-                    <Slider {...settings} >
-                        {myProjects.map((project, i) => <Project key={i} project={project} />)}
-                    </Slider>
+            <header className="md:mx-10 flex">
+                Projects
+                <img src={swipe} alt="swipe indicator" className="ml-2 w-5 xl:hidden" />
+            </header>
+            <article>
+                <Slider {...settings} >
+                    {myProjects.map((project, i) => <Project key={i} project={project} />)}
+                </Slider>
             </article>
         </section>
     )
