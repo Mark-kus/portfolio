187319cf91d7eecd78077a69a984255aba5234f5
Marco <tignanellimarco@gmail.com>
2023-05-30 15:51:16 -0300

    styling

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ export default function Project({ project }) {
     const clsImage = toggle ? 'md:scale-105' : ''
 
     return (
-        <article className="md:w-[400px] md:h-60 border md:rounded-lg md:mx-auto overflow-hidden"
+        <article className="xl:w-[400px] md:w-[375px] md:h-60 border md:rounded-lg md:mx-auto overflow-hidden"
             onMouseEnter={showToggle}
             onMouseLeave={hideToggle}>
 
@@ -19,7 +19,7 @@ export default function Project({ project }) {
             <div className={`flex justify-between bg-slate-600`}>
                 <h1 className="p-1">{project.name}</h1>
                 <a href={project.deploy || project.repo} target="_blank"
-                className="p-1 bg-slate-800">{project.deploy ? 'Deploy' : 'Repository'}</a>
+                className="p-1 bg-slate-800 hover:bg-slate-950">{project.deploy ? 'Go to deploy' : 'Go to repository'}</a>
             </div>
 
         </article>
