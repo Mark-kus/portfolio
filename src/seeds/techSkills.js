28c71ee9fc1fc52de1f11b655b6c09686ef96a32
Marco <tignanellimarco@gmail.com>
2023-05-30 16:50:07 -0300

    tailwind added

--- src/seeds/techSkills.js
+++ src/seeds/techSkills.js
@@ -8,35 +8,41 @@ import react from '../assets/tech/react.svg'
 import redux from '../assets/tech/redux.svg'
 import sequelize from '../assets/tech/sequelize.svg'
 import mongodb from '../assets/tech/mongodb.svg'
+import tailwind from '../assets/tech/tailwind.svg'
 
-export default [{
-    tech: html,
-    name: 'HTML'
-}, {
-    tech: css,
-    name: 'CSS'
-}, {
-    tech: javascript,
-    name: 'Javascript'
-}, {
-    tech: nodejs,
-    name: 'Node.JS'
-}, {
-    tech: react,
-    name: 'React.JS'
-}, {
-    tech: nextjs,
-    name: 'Next.JS'
-}, {
-    tech: redux,
-    name: 'Redux.JS'
-}, {
-    tech: sequelize,
-    name: 'Sequelize'
-}, {
-    tech: postgres,
-    name: 'PostgreSQL'
-}, {
-    tech: mongodb,
-    name: 'MongoDB'
-}]
+export default [
+    {
+        tech: html,
+        name: 'HTML'
+    }, {
+        tech: css,
+        name: 'CSS'
+    }, {
+        tech: javascript,
+        name: 'Javascript'
+    }, {
+        tech: nodejs,
+        name: 'Node.JS'
+    }, {
+        tech: react,
+        name: 'React.JS'
+    }, {
+        tech: nextjs,
+        name: 'Next.JS'
+    }, {
+        tech: redux,
+        name: 'Redux.JS'
+    }, {
+        tech: sequelize,
+        name: 'Sequelize'
+    }, {
+        tech: postgres,
+        name: 'PostgreSQL'
+    }, {
+        tech: mongodb,
+        name: 'MongoDB'
+    }, {
+        tech: tailwind,
+        name: 'TailwindCSS'
+    }
+]
