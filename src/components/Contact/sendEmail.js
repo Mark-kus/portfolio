df9c75d4cdf52971a3934373c4a3a3fe75d21952
Marco <tignanellimarco@gmail.com>
2023-05-30 22:55:05 -0300

    mail sending fix and toasts added

--- src/components/Contact/sendEmail.js
+++ src/components/Contact/sendEmail.js
@@ -1,16 +1,33 @@
 import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+
 const {
-    EMAIL_TEMPLATE,
-    EMAIL_SERVICE,
-    EMAIL_KEY
+    VITE_EMAIL_TEMPLATE,
+    VITE_EMAIL_SERVICE,
+    VITE_EMAIL_KEY
 } = import.meta.env
 
-export default (inputs, setInputs) => {
-    console.log(inputs);
-    emailjs.send(EMAIL_SERVICE, EMAIL_TEMPLATE, {
+export default (inputs, setInputs, setSending) => {
+    
+    emailjs.send(VITE_EMAIL_SERVICE, VITE_EMAIL_TEMPLATE, {
         from_name: inputs.name,
         message: inputs.message,
         from_email: inputs.email,
-    });
-    setInputs({ name: '', email: '', message: '' })
+    }, VITE_EMAIL_KEY)
+
+        .then(() => {
+            toast.success("Mail sent!", {
+                autoClose: 4000,
+                theme: "dark",
+            })
+            setInputs({ name: '', email: '', message: '' })
+            setSending(false)
+        })
+        .catch(() => {
+            toast.error("An error ocurred!", {
+                autoClose: 4000,
+                theme: "dark",
+            })
+            setSending(false)
+        })
 }
