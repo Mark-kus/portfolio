0985afbdd2a116dd5e54791ccfedf60b25ddf7fd
Marco Tignanelli <tignanellimarco@gmail.com>
2024-12-07 12:11:09 -0300

    fix projects alert

--- src/components/ProjectsAlert.jsx
+++ src/components/ProjectsAlert.jsx
@@ -18,27 +18,27 @@ const ProjectsAlert = ({ dictionary }) => {
   };
 
   return (
-    <button
-      className="relative w-full"
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-    >
-      <p className="border-2 border-orange-400 text-orange-400 bg-red-200 bg-opacity-20 dark:border-yellow-300 dark:text-yellow-300 dark:bg-yellow-200 dark:bg-opacity-20 rounded-full w-12 h-12 flex justify-center items-center font-semibold">
+    <div className="relative w-full">
+      <button
+        className="border-2 border-orange-400 text-orange-400 bg-red-200 bg-opacity-20 dark:border-yellow-300 dark:text-yellow-300 dark:bg-yellow-200 dark:bg-opacity-20 rounded-full w-12 h-12 flex justify-center items-center font-semibold focus:ring-2 transition-all"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
         !
-      </p>
+      </button>
       <div
         className={`-rotate-90 absolute w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-orange-400 dark:border-b-yellow-300 top-5 left-11 transition-opacity ${
           showPopover ? "opacity-100" : "opacity-0"
         }`}
       />
-      <div
-        className={`absolute bg-orange-400 dark:bg-yellow-300 p-2 bottom-1 left-[3.4rem] rounded-lg text-black z-10 w-[85%] min-[1170px]:w-96 transition-opacity ${
+      <p
+        className={`absolute bg-orange-400 dark:bg-yellow-300 p-2 -bottom-5 md:bottom-1 left-[3.4rem] rounded-lg text-black z-10 w-[85%] min-[1170px]:w-96 transition-opacity ${
           showPopover ? "opacity-100" : "opacity-0"
         }`}
       >
         {dictionary.alert}
-      </div>
-    </button>
+      </p>
+    </div>
   );
 };
 
