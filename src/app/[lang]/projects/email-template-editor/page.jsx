8cc39b54c742a36dba98250412b46634d2633286
Mark-kus <tignanellimarco@gmail.com>
2025-02-06 23:37:02 -0300

    Add metadata to email template editor

--- src/app/[lang]/projects/email-template-editor/page.jsx
+++ src/app/[lang]/projects/email-template-editor/page.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import Main from "@/containers/email-template-editor/Main";
 
+export const metadata = {
+  title: "Email Template Editor",
+};
+
 export default async function EmailTemplateEditor({ params: { lang = "en" } }) {
   const dictionary = await import(`@/app/dictionaries/${lang}.json`).then(
     (m) => m.default
